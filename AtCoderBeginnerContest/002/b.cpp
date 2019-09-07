741551ef0fdd3b6186f06c9abd25f31aee13fc3b
morishitamakoto0330 <morishita0330@gmail.com>
2019-09-07 16:34:49 +0900

    [solve] ABC002 B

--- AtCoderBeginnerContest/002/b.cpp
+++ AtCoderBeginnerContest/002/b.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(void) {
-	int X, Y, ans;
-	cin >> X >> Y;
-	ans = X > Y ? X : Y;
+	int i;
+	string W, ans;
+	// get input ------------------------
+	cin >> W;
+	ans = "";
+	// exclude "aiueo" ------------------
+	for (i = 0; i < W.size(); i++) {
+		if (W[i] != 'a'
+			&& W[i] != 'i'
+			&& W[i] != 'u'
+			&& W[i] != 'e'
+			&& W[i] != 'o'
+		) {
+			ans += W[i];
+		}
+	}
+	// disp answer ---------------------
 	cout << ans << endl;
 	return 0;
 }
