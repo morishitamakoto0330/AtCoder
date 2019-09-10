5aa9b5c2f4104f508b85a363e213498b498bbaf0
morishitamakoto0330 <morishita0330@gmail.com>
2019-09-10 15:00:06 +0900

    [solve] ABC042 C

--- AtCoderBeginnerContest/042/c.cpp
+++ AtCoderBeginnerContest/042/c.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 int main(void) {
-    int N, K, i, tmp, ans;
+    int N, K, i, j, tmp, ans, digits;
+    bool hates;
     vector<int> D;
     // get input --------------------------------
     cin >> N >> K;
@@ -13,8 +15,25 @@ int main(void) {
         D.push_back(tmp);
     }
     // search ----------------------------------
-    ans = N;
-    
+    ans = N-1;
+    do {
+        hates = false;
+        ans++;
+        // get N digit -----------------------
+        digits = 1;
+        tmp = ans;
+        while((tmp=tmp/10) != 0) digits++;
+        // check ----------------------------
+        for(i = 0; i < digits; i++) {
+            tmp = ans%(int)(round(pow(10, (i + 1))));
+            tmp /= (int)(round(pow(10, i)));
+            for(j = 0; j < K; j++) {
+                if(D[j] == tmp) {
+                    hates = true;
+                }
+            }
+        }
+    } while(hates);
     // disp answer ------------------------------
     cout << ans << endl;
     return 0;
