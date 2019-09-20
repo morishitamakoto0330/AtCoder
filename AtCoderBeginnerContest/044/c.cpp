7de83d274ed186f0941beacc68a6c795cc8806a4
morishitamakoto0330 <morishita0330@gmail.com>
2019-09-21 02:03:12 +0900

    [fail] ABC044 C

--- AtCoderBeginnerContest/044/c.cpp
+++ AtCoderBeginnerContest/044/c.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 #define ARRAY_NUM 100
 #define OFFSET 50
@@ -6,9 +7,9 @@
 using namespace std;
 
 int main(void) {
-    unsigned long long ans;
+    unsigned long long ans, pair_num, A_num;
     int i, tmp;
-    int N, A, A_NUM;
+    int N, A;
     int diff[ARRAY_NUM];
     // init diff array -----------------------
     for(i = 0; i < ARRAY_NUM; i++) {
@@ -23,11 +24,23 @@ int main(void) {
     }
     // count answer --------------------------
     ans = 0;
-    A_NUM = diff[OFFSET];
+    A_num = diff[OFFSET];
+	pair_num = 0;
     for(i = 1; i < OFFSET; i++) {
         if(diff[i] > 0 && diff[ARRAY_NUM - i] > 0) {
-            ans += diff[i]*diff[ARRAY_NUM - i];
+            pair_num += diff[i]*diff[ARRAY_NUM - i];
         }
     }
+	for (i = 0; i < OFFSET; i++) {
+		if (diff[i] > 0) {
+			cout << i << ":" <<  diff[i];
+			cout << " " << ARRAY_NUM - i << ":" << diff[ARRAY_NUM - i];
+			cout << endl;
+		}
+	}
+	ans = pow(2, A_num);
+	ans = ans * (pair_num - 1);
+	// disp answer ----------------------------
+	cout << ans << endl;
     return 0;
 }
