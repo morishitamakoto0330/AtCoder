f68c4d6a3fd0b82ff774969290b57d8dca48408d
morishitamakoto0330 <morishita0330@gmail.com>
2019-09-29 15:28:03 +0900

    [solve] ABC048 B

--- AtCoderBeginnerContest/048/b.cpp
+++ AtCoderBeginnerContest/048/b.cpp
@@ -2,15 +2,24 @@
 
 using namespace std;
 
+long long f(long long n, long long x);
+
 int main(void) {
-	long long count;
+	long long ans;
 	long long i;
 	long long a, b, x;
 	// get input ----------------------------------
 	cin >> a >> b >> x;
-	// count --------------------------------------
-	count = (b - a + 1) / x;
+	// calc ---------------------------------------
+	ans = f(b, x) - f(a - 1, x);
 	// disp answer ---------------------------------
-	cout << count << endl;
+	cout << ans << endl;
 	return 0;
+}
+
+long long f(long long n, long long x) {
+	if (n == -1) {
+		return 0;
+	}
+	return n / x + 1;
 }
